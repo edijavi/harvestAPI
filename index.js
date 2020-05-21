3fef7b257b95d41bac16dd4cab2ea691d2184344
Edison Lamar <edilata@msn.com>
2020-05-21 03:06:49 +0200

    conecction with the mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
+//  local connection to db
+// 'mongodb://localhost:27017/harvestDB'
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/curso_mean2', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+mongoose.connect('mongodb+srv://harvest:easv2020@harvest-app-gupi2.mongodb.net/harvestDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false }, (err, res) => {
   if (err){
     throw err;
   }else{
