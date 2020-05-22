d84ebf840e73b3e8bb7a54b598e7a42ca1895c0c
Edison Lamar <edilata@msn.com>
2020-05-22 09:48:05 +0200

    Headers

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //configurar cabeceras http
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-Whith, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, DELETE');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-Whith, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, DELETE');
 
-//   next();
-// });
+  next();
+});
 
 //rutas base
 app.use('/api', user_routes);
