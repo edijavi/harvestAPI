ecec29a0bb8357b98e2a9fa47e46e2dbaea3b646
Edison Lamar <edilata@msn.com>
2020-05-22 09:02:40 +0200

    headers deleted

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 'use strict'
 var express = require('express');
 var bodyParser = require('body-parser');
-// var cors = require('cors');
+var cors = require('cors');
 var app = express();
 
 //cargar rutas
 var user_routes = require('./routes/user');
-// app.use(cors());
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //configurar cabeceras http
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-Whith, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, DELETE');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-Whith, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
+//   res.header('Allow', 'GET, POST, OPTIONS, DELETE');
 
-  next();
-});
+//   next();
+// });
 
 //rutas base
 app.use('/api', user_routes);
