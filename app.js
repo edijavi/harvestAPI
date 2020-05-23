64a874a42799768439673761ea4a45826cd810ca
Edison Lamar <edilata@msn.com>
2020-05-23 10:51:46 +0200

    comments

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ app.use(cors());
 const user_routes = require('./routes/user');
 
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
-//rutas base
+//Bese routes
 app.use('/api', user_routes);
 
 
--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://harvest:easv2020@harvest-app-gupi2.mongodb.net/harvestDB?retryWrites=true&w=majority', 
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, (err, res) => {
-  if (err){
+mongoose.connect('mongodb+srv://harvest:easv2020@harvest-app-gupi2.mongodb.net/harvestDB?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}, (err, res) => {
+  if (err) {
     throw err;
-  }else{
+  } else {
     console.log("La base de datos está corriendo correctamente");
 
-    app.listen(port, function (){
+    app.listen(port, function () {
       console.log(`Servidor del API rest de música escuchando en http://localhost:${port}`);
-    });    
+    });
   }
 });
